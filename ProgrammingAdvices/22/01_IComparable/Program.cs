87df9b6a74a7a488b6c31bd872f4ddf49366b8e9
CSXV <maliksharif@hotmail.com>
2025-05-29 12:09:27 +0300

    add IComparable problem

---
+++ ProgrammingAdvices/22/01_IComparable/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+
+public class Person : IComparable<Person>
+{
+  public string firstName { get; set; }
+  public int Age { get; set; }
+
+  public Person(string firsname, int age)
+  {
+    this.firstName = firsname;
+    this.Age = age;
+  }
+
+  public int CompareTo(Person otherPeron)
+  {
+    if (otherPeron == null) return 1;
+
+    return this.Age.CompareTo(otherPeron.Age);
+  }
+
+  public override string ToString()
+  {
+    return $"{firstName}, {Age} years old";
+  }
+}
+
+public class Program
+{
+  static void Main()
+  {
+    List<Person> people = new List<Person> {
+    new Person("foo", 21),
+    new Person("bar", 20),
+    new Person("baz", 30),
+    new Person("qux", 28),
+    };
+
+    people.Sort();
+
+    foreach (var Person in people)
+    {
+      Console.WriteLine(Person.ToString());
+    }
+  }
+}
